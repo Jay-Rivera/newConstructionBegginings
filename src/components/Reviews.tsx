d366bd8fd5323e96fde96291e0666f1a1ccbe94c
Jose Rivera <jay.rivera1996@gmail.com>
2025-05-10 14:40:11 -0700

    Small Bug fix

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -3,7 +3,7 @@ import GoogleReviewsWidget from "./GoogleReviewsWidget";
 function Reviews() {
   return (
     <div className="reviews__section">
-      <h2>See what our customers have to say :</h2>
+      <h2>See what our customers have to say</h2>
       <GoogleReviewsWidget />
     </div>
   );
